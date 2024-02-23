d5ae02d5ac564da2fea2ef3c3932f951e4d8930f
lauti44 <lauti44games@gmail.com>
2024-02-23 00:01:02 -0300

    updated AudioScripts

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using FMOD.Studio;
 
 public class AudioManager : MonoBehaviour
 {
+    private List<EventInstance> eventInstances;
+    private List<StudioEventEmitter> eventEmitters;
+
+    private EventInstance ambienceEventInstance;
+
+    private EventInstance musicEventInstance;
     public static AudioManager instance { get; private set; }
 
     private void Awake()
@@ -17,11 +24,72 @@ public class AudioManager : MonoBehaviour
         {
             instance = this;
         }
+        eventInstances = new List<EventInstance>();
+        eventEmitters = new List<StudioEventEmitter>();
     }
 
+    private void Start()
+    {
+        //InitializeAmbience(FMODEvents.instance.ambience);
+        InitializeMusic(FMODEvents.instance.music);
+    }
+
+    private void InitializeAmbience(EventReference ambienceEventReference)
+    {
+        ambienceEventInstance = CreateEventInstance(ambienceEventReference);
+        ambienceEventInstance.start();
+    }
+
+    private void InitializeMusic(EventReference musicEventReference)
+    {
+        musicEventInstance = CreateEventInstance(musicEventReference);
+        musicEventInstance.start();
+    }
+
+    public void SetAmbienceParameter(string parameterName, float parameterValue)
+    {
+        ambienceEventInstance.setParameterByName(parameterName, parameterValue);
+    }
 
     public void PlayOneShot(EventReference sound, Vector3 worldPosition)
     {
         RuntimeManager.PlayOneShot(sound, worldPosition);
+    }
+
+    public EventInstance CreateEventInstance(EventReference eventReference)
+    {
+        EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        eventInstances.Add(eventInstance);
+        return eventInstance;
+
+    }
+    
+    public StudioEventEmitter InitializeEventEmitter(EventReference eventReference, GameObject emitterGameObject)
+    {
+        StudioEventEmitter emitter = emitterGameObject.GetComponent<StudioEventEmitter>(); 
+        emitter.EventReference = eventReference;
+        eventEmitters.Add(emitter);
+        return emitter;
+    }
+
+    private void CleanUp()
+    {
+        // stop and release any created instances
+        foreach (EventInstance eventInstance in eventInstances)
+        {
+            eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            eventInstance.release();
+        }
+
+        // stop all of the event emitters if we don't may hang around in other scenes
+        foreach(StudioEventEmitter emitter in eventEmitters)
+        {
+            emitter.Stop();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        CleanUp();
     }
 }
--- Assets/Scripts/Audio/FMODEvents.cs
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -2,20 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using Unity.Collections.LowLevel.Unsafe;
 
 public class FMODEvents : MonoBehaviour
 {
     public static FMODEvents instance { get; private set; }
 
-    //EXAMPLE
-    [field: Header("Coin SFX")]
-    [field: SerializeField] public EventReference coinCollected { get; private set; }
+    [field: Header("Music")]
+    [field: SerializeField] public EventReference music { get; private set; }
+
+    [field: Header("Connect Target and Laser SFX")]
+    [field: SerializeField] public EventReference connectTargetLaser { get; private set; }
+
+    [field: Header("Disconnect Target and Laser SFX")]
+    [field: SerializeField] public EventReference disconnectTargetLaser { get; private set; }
+    
+    [field: Header("Button Select SFX")]
+    [field: SerializeField] public EventReference buttonSelect { get; private set; }
+
+    
+    
+    [field: Header("-Collect SFX")]
+    [field: SerializeField] public EventReference collect { get; private set; }
+
+
+    [field: Header("-Level Complete SFX")]
+    [field: SerializeField] public EventReference levelComplete { get; private set; }
+
+
+    [field: Header("-Player Death SFX")]
+    [field: SerializeField] public EventReference playerDeath { get; private set; }
+
+
+    [field: Header("-Taking Damage SFX")]
+    [field: SerializeField] public EventReference takingDamage { get; private set; }
+
+
+
 
     private void Awake()
     {
         if (instance != null)
         {
             Debug.LogError("\"Found more than one FMOD Events instance in the scene.");
+        }
+        else
+        {
+            instance = this;
         }
     }
 }
