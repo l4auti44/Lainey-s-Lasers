8bb94f512b73d9f3cd9c5b28ceff420d60eed865
lauti44 <lauti44games@gmail.com>
2024-04-21 18:48:40 -0300

    fixed boxCatcher bugs

--- Assets/Scripts/BoxCatcher.cs
+++ Assets/Scripts/BoxCatcher.cs
@@ -3,11 +3,23 @@ using UnityEngine;
 [RequireComponent(typeof(Target))]
 public class BoxCatcher : MonoBehaviour
 {
+    private int objectsAmount = 0;
     private void OnTriggerEnter(Collider other)
     {
+        objectsAmount++;
         if (other.CompareTag("Triggerer"))
         {
             this.GetComponent<Target>().DoAction();
         }
     }
+    private void OnTriggerExit(Collider other)
+    {
+        objectsAmount--;
+        if (other.CompareTag("Triggerer") && objectsAmount == 0)
+        {
+            this.GetComponent<Target>().Undo();
+        }
+    }
+
+
 }
